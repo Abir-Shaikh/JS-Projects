afa6e86406a6a56bced560ab6a2ecf708b5c61ce
Abir Shaikh <shaikhabir044@gmail.com>
2026-01-27 11:07:27 +0530

    simple message display

---
+++ simple_Message_Display/script.js
@@ -0,0 +1,16 @@
+function display1(){
+    let a = document.getElementById("message1")
+    a.innerHTML = "Button was clicked! Message displayed!"
+}
+function display2(){
+    let a = document.getElementById("message2")
+    a.innerHTML = "Button was clicked! Message displayed!"
+}
+function display3(){
+    let a = document.getElementById("message3")
+    a.innerHTML = "Button was clicked! Message displayed!"
+}
+function display4(){
+    let a = document.getElementById("message4")
+    a.innerHTML = "Button was clicked! Message displayed!"
+}
