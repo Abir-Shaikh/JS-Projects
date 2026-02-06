b26a0f14acd3b2099db403a8978ace3ddfdbe794
Abir Shaikh <shaikhabir044@gmail.com>
2026-02-06 22:52:18 +0530

    js drum kit

---
+++ Drum-Kit/src/index.js
@@ -0,0 +1,33 @@
+window.addEventListener("keydown",function(event){
+    const audio = this.document.querySelector(
+        `audio[data-key="${event.keyCode}"]`
+    );
+
+    console.log(event.keyCode);
+
+    const key = this.document.querySelector(
+        `.key[data-key="${event.keyCode}"]`
+    );
+
+    if(!audio)return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add("playing");
+
+});
+
+function removeTransition(event){
+        if(event.propertyName !== "transform") return;
+        this.classList.remove("playing");
+    }
+
+const keys = this.document.querySelectorAll(".key");
+keys.forEach(key => 
+key.addEventListener("transitionend", removeTransition)
+);
+
+
+
+
